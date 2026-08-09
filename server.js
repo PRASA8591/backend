06b1a719c8f4e78d14823554e84d598210e3037b
PRASA8591 <punsaraprasanna@gmail.com>
2026-08-09 12:46:33 +0530

    000000000000000

--- server.js
+++ server.js
@@ -13,7 +13,13 @@ const notificationRoutes = require('./routes/notificationRoutes');
 const app = express();
 
 // Middlewares
-app.use(cors());
+app.use(cors({
+  origin: true,
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
+}));
+app.options('*', cors());
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
